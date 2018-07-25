46f8879355a0c547161d6f2744fcb8a52c5c9b5e
Chris Streeter <chris@educreations.com>
2018-07-25 08:21:39 -0700

    Updated dateutil requirements

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     install_requires=[
         'nydus>=0.10.6',
         'redis>=2.7.2',
-        'python-dateutil==1.5',
+        'python-dateutil>=2.1,<3.0',
     ],
     tests_require=[
         'nose>=1.0',
